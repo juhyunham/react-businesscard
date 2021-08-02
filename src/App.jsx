3b32ae5b125b0c589a2e8335a9f6f1257655163b
juhyunham <judev0227@gmail.com>
2021-08-02 16:05:39 +0900

    add basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Maker from './components/maker/maker'
 function App({ FileInput, authService, cardRepository}) {
   return (
     <div className={styles.app}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact={true} path="/">
             <Login authService = {authService}/>
