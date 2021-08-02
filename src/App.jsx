028662399cff32c91cc137f2d8ce0c7b73666679
juhyunham <judev0227@gmail.com>
2021-08-02 16:19:14 +0900

    update basename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Maker from './components/maker/maker'
 function App({ FileInput, authService, cardRepository}) {
   return (
     <div className={styles.app}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename="/react-businesscard">
         <Switch>
           <Link to="/"></Link>
           <Route exact={true} path="/">
